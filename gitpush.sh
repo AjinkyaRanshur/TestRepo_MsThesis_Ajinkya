18116ada662648d5bb3a020055d1c538bba856ab
AjinkyaRanshur <ranshur.ajinkya.res@gmail.com>
2025-05-21 17:27:20 +0200

    File Removed and New file added

--- gitpush.sh
+++ gitpush.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 ###########################
 
+echo What message do you want to put?
+read message
+
+#########################
+
 # add all added/modified files
 git add *
 # commit changes
-git commit -m "made changes"
+git commit -m "$message"
 # push to git remote repository
 git push origin main
 ###########################
