6fbd2eb4e6e6a6abecd845f7a692efab8f10a9f1
AjinkyaRanshur <ranshur.ajinkya.res@gmail.com>
2025-05-21 17:18:20 +0200

    made changes

---
+++ gitpush.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+###########################
+
+# add all added/modified files
+git add *
+# commit changes
+git commit -m "made changes"
+# push to git remote repository
+git push origin main
+###########################
+echo Press Enter...
+read
+
