3df1e1486d4d60cd803406e060414a29e852c123
Ajinkya_Ranshur <ranshur.ajinkya.res@gmail.com>
2025-05-26 01:23:35 +0200

    code_restructered_and_model_viz_added

--- week1/simple_model.py
+++ week1/simple_model.py
@@ -7,20 +7,20 @@ import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-
+from torch.utils.tensorboard import SummaryWriter
 
 #Normalizing the images
 transform= transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5))])
 
 batch_size=4
 
-trainset=torchvision.datasets.CIFAR10(root='/home/ajinkya/projects/datasets/cifardataset',train=True,download=True,transform=transform)
+trainset=torchvision.datasets.CIFAR10(root='D:\datasets',train=True,download=True,transform=transform)
 
-trainloader=torch.utils.data.DataLoader(trainset,batch_size=batch_size,shuffle=True,num_workers=4)
+trainloader=torch.utils.data.DataLoader(trainset,batch_size=batch_size,shuffle=True,num_workers=0)
 
-testset=torchvision.datasets.CIFAR10(root='/home/ajinkya/projects/datasets/cifar10dataset',train=False,download=True,transform=transform)
+testset=torchvision.datasets.CIFAR10(root='D:\datasets',train=False,download=True,transform=transform)
 
-testloader=torch.utils.data.DataLoader(testset,batch_size=batch_size,shuffle=False,num_workers=4)
+testloader=torch.utils.data.DataLoader(testset,batch_size=batch_size,shuffle=False,num_workers=0)
 
 classes= ('plane','car','bird','cat','deer','dog','frog','horse','ship','truck')
 
@@ -44,68 +44,80 @@ class Net(nn.Module):
         x=self.fc3(x)
         return x
 
+def evaluation_metric(net):
+    # Testing
+    net.eval()
+    total_correct = 0
+    total_samples = 0
 
-net=Net()
+    with torch.no_grad():
+        for batch_idx, batch in enumerate(testloader):
+            images, labels = batch
+            outputs = net(images)
+            _, predicted = torch.max(outputs, 1)
+            total_correct += (predicted == labels).sum().item()
+            total_samples += labels.size(0)
 
-criterion=nn.CrossEntropyLoss()
-optimizer=optim.SGD(net.parameters(),lr=0.001,momentum=0.9)
-
-#Training Module 
-#We start with some epochs and then iterate through each batch in the trainloader we calculate the loss for each image and then average it over each epoch 
-
-loss_arr=[]
-for epoch in range(4):
-    
-    running_loss=[]
-    #running_loss=np.array(running_loss)
-    for batch_idx,batch in enumerate(trainloader):
-        images,labels=batch
-        optimizer.zero_grad()
-        ypred=net(images)
-        loss=criterion(ypred,labels)
-        #Used to compute the gradients of the network
-        loss.backward()
-        #This is used to update the weights of the network
-        optimizer.step()
-        #print(type(loss.item()))
-        running_loss.append(loss.item())
-        
-    avg_loss=np.mean(running_loss)
-    print(f"Epoch:{epoch} and AverageLoss:{avg_loss}")
-    loss_arr.append(avg_loss)
-
-#Testing Model
-
-#Set the model into evaluation mode
-net.eval() 
-
-#for epoch in range(4):
-    
-total_correct=0
-total_samples=0
-
-with torch.no_grad():
-    for batch_idx,batch in enumerate(testloader):
-        images,labels=batch
-        outputs=net(images)
-        #torch.max outputs the probability along specified dimension in this case 1
-        _,predicted=torch.max(outputs,1)
-        total_correct+=(predicted == labels).sum().item()
-        total_samples+=labels.size(0)
-
-accuracy=100 * (total_correct/total_samples)
-print(f'Accuracy = {accuracy:.2f}%')
-
-#print(loss_arr)
-iters=range(1,5)
-
-plt.figure(figsize=(8,6))
-plt.plot(iters,loss_arr,linewidth=2,markersize=6)
-plt.title('Training Evaluation')
-plt.xlabel("Epochs")
-plt.ylabel("Average Loss")
-plt.xticks(iters)
-plt.tight_layout()
-plt.savefig('tr_loss.png')
+    accuracy = 100 * (total_correct / total_samples)
 
 
+    return accuracy
+
+def plot_metrics(x,y):
+
+    # Plotting
+    plt.figure(figsize=(8, 6))
+    plt.plot(x, y, linewidth=2, markersize=6)
+    plt.title('Training Evaluation')
+    plt.xlabel("Epochs")
+    plt.ylabel("Average Loss")
+    plt.xticks(x)
+    plt.tight_layout()
+    plt.savefig('avgloss_vs_epoch.png')
+
+    return True
+
+
+def fwd_training(net,criterion):
+    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+    loss_arr = []
+    for epoch in range(4):
+        running_loss = []
+        for batch_idx, batch in enumerate(trainloader):
+            images, labels = batch
+            optimizer.zero_grad()
+            ypred = net(images)
+            loss = criterion(ypred, labels)
+            loss.backward()
+            optimizer.step()
+            running_loss.append(loss.item())
+
+        avg_loss = np.mean(running_loss)
+        print(f"Epoch:{epoch} and AverageLoss:{avg_loss}")
+        loss_arr.append(avg_loss)
+
+    accuracy=evaluation_metric(net)
+    iters = range(1, 5)
+    plot_bool=plot_metrics(iters,loss_arr)
+    if plot_bool==True:
+        print("Plots Successfully Stored")
+    print(f'Accuracy = {accuracy:.2f}%')
+
+def main():
+    # Your training and testing code goes here
+    net = Net()
+    criterion = nn.CrossEntropyLoss()
+    fwd_training(net,criterion)
+    writer = SummaryWriter('runs/cifar10_experiment')
+    sample_input = torch.randn(1, 3, 32, 32)
+    writer.add_graph(net, sample_input)
+    writer.close()  
+
+
+
+
+
+
+# This line ensures safe multiprocessing
+if __name__ == "__main__":
+    main()
