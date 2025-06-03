af2a675456fb61bdb47c4570fc9fcd773d022e06
AjinkyaRanshur <ranshur.ajinkya.res@gmail.com>
2025-06-03 18:48:28 +0200

    Back_train updated

--- week1/modular_code/config.py
+++ week1/modular_code/config.py
@@ -1,5 +1,5 @@
 batch_size=4
-epochs=12
+epochs=10
 seed=42
 lr=0.001
 momentum=0.9
--- week1/modular_code/network.py
+++ week1/modular_code/network.py
@@ -25,29 +25,33 @@ class Net(nn.Module):
         self.deconv2_fb=nn.ConvTranspose2d(in_channels=16,out_channels=6,kernel_size=5,stride=1,padding=2)
         self.deconv1_fb=nn.ConvTranspose2d(in_channels=6,out_channels=3,kernel_size=5,stride=1,padding=2)
         self.unpool=nn.MaxUnpool2d(kernel_size=2, stride=2)
-
+        self.upsample=nn.Upsample(scale_factor=2,mode='bilinear')
+        self.upsample_nearest=nn.Upsample(scale_factor=2,mode='nearest')
 
     def feedforward_pass(self, x):
 
-        ft_AB,indices_AB = self.pool(F.relu(self.conv1(x)))
-        ft_BC,indices_BC = self.pool(F.relu(self.conv2(ft_AB)))
-        ft_BC_flat = torch.flatten(ft_BC, 1)  # Flatten all dimensions except batch
-        ft_CD = F.relu(self.fc1(ft_BC_flat))
-        ft_DE = F.relu(self.fc2(ft_CD))
-        output = self.fc3(ft_DE)
+        ft_AB = self.conv1(x)
+        pooled_ft_AB,indices_AB=self.pool(F.relu(ft_AB))
+        ft_BC = self.conv2(pooled_ft_AB)
+        pooled_ft_BC,indices_BC=self.pool(F.relu(ft_BC))
+        ft_BC_flat = torch.flatten(pooled_ft_BC, 1)  # Flatten all dimensions except batch
+        ft_CD = self.fc1(ft_BC_flat)
+        relu_CD=F.relu(ft_CD)
+        ft_DE = self.fc2(relu_CD)
+        relu_DE=F.relu(ft_DE)
+        output = self.fc3(relu_DE)
 
         return ft_AB, ft_BC, ft_CD, ft_DE, output,indices_AB,indices_BC
     
     def feedback_pass(self,output,indices_AB,indices_BC,ft_AB,ft_BC,ft_CD,ft_DE):   
 
         ft_ED=self.fc3_fb(output)
-        ft_DC=F.relu(self.fc2_fb(ft_DE))
-        ft_CB=F.relu(self.fc1_fb(ft_CD))
+        ft_DC=self.fc2_fb(ft_DE)
+        ft_CB=self.fc1_fb(ft_CD)
         ft_CB = ft_CB.view(-1, 16, 8, 8)
+        ft_CB=self.upsample_nearest(ft_CB)
         # This matches the forward pass structure correctly
-        ft_BC_unpool = F.relu(self.unpool(ft_BC, indices_BC))  # Reconstruct from 8x8 to 16x16
-        ft_BA = self.deconv2_fb(ft_BC_unpool)          # Then apply deconv
-        ft_BA_unpooled = F.relu(self.unpool(ft_BA, indices_AB)) # Reconstruct from 16x16 to 32x32
-        x = self.deconv1_fb(ft_BA_unpooled)              # Final deconv
+        ft_BA = self.deconv2_fb(self.upsample(ft_BC))        
+        x = self.deconv1_fb(ft_AB)              # Final deconv
 
         return ft_BA, ft_CB, ft_DC, ft_ED, x
