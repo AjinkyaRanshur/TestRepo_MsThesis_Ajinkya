caf8116c270ba5544f505a537290d0ae235de386
Ajinkya Ranshur <rashur.ajinkya.res@gmail.com>
2025-06-13 15:56:59 +0200

    epoch10

--- week2/config.py
+++ week2/config.py
@@ -1,13 +1,13 @@
 import torch
 batch_size=4
-epochs=5
-seed=30
+epochs=10
+seed=1
 lr=0.001
 momentum=0.9
-#training_condition="ff_fb_train"
-training_condition="pc_train"
+training_condition="ff_fb_train"
+#training_condition="pc_train"
 device=torch.device("cuda" if torch.cuda.is_available() else "cpu")
-timesteps=4
+timesteps=10
 
 
 
