1b3be1de75e4c2d739c1791b584516abe608c04c
Ajinkya Ranshur <rashur.ajinkya.res@gmail.com>
2025-06-13 16:59:56 +0200

    week2 update

--- week2/config.py
+++ week2/config.py
@@ -8,6 +8,19 @@ training_condition="ff_fb_train"
 #training_condition="pc_train"
 device=torch.device("cuda" if torch.cuda.is_available() else "cpu")
 timesteps=10
+load_model=False
+save_model=True
+#hyp_dict={'Gamma= 0.3,0.3,0.3\n Beta=0.3,0.3,0.3\n alpha=0.01,0.01,0.01\n ':
+#        [[0.3,0.3,0.3],[0.3,0.3,0.3],[0.01,0.01,0.01]}
+
+hyp_dict={'Gamma= 0.4,0.2,0.8\n Beta=0.5,0.3,0.2\n alpha=0.01,0.01,0.01\n ':
+        [[0.4,0.2,0.8],[0.5,0.3,0.2],[0.01,0.01,0.01]],
+        'Gamma= 0.2,0.2,0.2\n Beta=0.2,0.4,0.5\n alpha=0.01,0.01,0.01\n ':
+        [[0.2,0.2,0.2],[0.2,0.4,0.5],[0.01,0.01,0.01]],
+        'Gamma= 0.5,0.5,0.5\n Beta=0.5,0.5,0.5\n alpha=0.01,0.01,0.01\n ':
+        [[0.5,0.5,0.5],[0.5,0.5,0.5],[0.01,0.01,0.01]]}
+
+
 
 
 
