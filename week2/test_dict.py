0b0fe6d9460d2eeceac4614214bfa16cb889f063
AjinkyaRanshur <ranshur.ajinkya.res@gmail.com>
2025-06-13 13:48:10 +0200

    saltandpep

--- week2/test_dict.py
+++
@@ -1,5 +0,0 @@
-hyp_dict={'Gamma= 0.2,0.7\n Beta=0.8,0.3\n alpha=0.3,0.8\n ':[[0.2,0.7],[0.8,0.3],[0.3,0.8]],'Gamma= 0.8,0.3\n Beta=0.2,0.37\n alpha=0.3,0.8\n ':[[0.8,0.3],[0.2,0.7],[0.3,0.8]],'Gamma= 0.5,0.5\n Beta=0.5,0.5\n alpha=0.3,0.8\n ':[[0.5,0.5],[0.5,0.5],[0.3,0.8]]}
-
-for key,value in hyp_dict.items():
-    print("Key",key,"Value",value)
-    print("\n")
