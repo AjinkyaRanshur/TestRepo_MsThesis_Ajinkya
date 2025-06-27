9c0aad13b06f39ebd396cfc59b30fa6c13e0f492
AjinkyaRanshur <ranshur.ajinkya.res@gmail.com>
2025-06-27 14:09:42 +0200

    predcode_shallow

--- week4/predcode_shallow/configs/config1.py
+++ week4/predcode_shallow/configs/config1.py
@@ -1,13 +1,13 @@
 import torch
 batch_size=128
-epochs=3
+epochs=10
 seed=42
 lr=0.001
 momentum=0.9
 #datasetpath='/home/ajinkya/projects/datasets'
 datasetpath='/home/ajinkyar/datasets'
-training_condition="ff_fb_train"
-#training_condition="pc_train"
+#training_condition="ff_fb_train"
+training_condition="pc_train"
 # Fixed device configuration
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"CUDA available: {torch.cuda.is_available()}")
@@ -28,5 +28,5 @@ gammaset=[[0.33,0.33,0.33,0.33]]
 betaset=[[0.33,0.33,0.33,0.33]]
 alphaset=[[0.01,0.01,0.01,0.01]]
 
-experiment_name="Test_Run_Net_fixed"
+experiment_name="Test_Run_small_batch"
 
