26a8d79d668829cfec3a5c43dbc767e755767ed9
AjinkyaRanshur <ranshur.ajinkya.res@gmail.com>
2025-09-29 14:26:19 +0200

    week 9 updated

---
+++ week9/illusion_pc_train.py
@@ -0,0 +1,83 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from eval_and_plotting import classification_accuracy_metric,classification_loss_metric,plot_metrics,recon_pc_loss,eval_pc_accuracy
+import os
+import wandb
+from wb_tracker import init_wandb
+from add_noise import noisy_img
+import torchvision.utils as vutils
+
+def illusion_pc_training(net,trainloader,testloader,pc_train_bool,config):
+
+    if pc_train_bool=="fine_tuning":
+        criterion=nn.CrossEntropyLoss()
+        optimizer= optim.Adam(list(net.fc1.parameters())+list(net.fc2.parameters())+list(net.fc3.parameters()), lr=config.lr)
+        loss_arr=[]
+        ##In zhoyang's paper finetuning was for only 25 epochs
+        for epoch in range(config.epochs):
+            running_loss=[]
+            val_recon_loss=[]
+            total_correct = np.zeros(config.timesteps + 1)  # ✅ Initialize here
+            total_samples = 0  # ✅ Initialize here
+            net.train()
+            for images,labels,_ in trainloader:
+                images,labels=images.to(config.device),labels.to(config.device).unsqueeze(1)
+                ft_AB_pc_temp = torch.zeros(config.batch_size, 6, 32, 32).to(config.device)
+                ft_BC_pc_temp = torch.zeros(config.batch_size, 16, 16, 16).to(config.device)
+                ft_CD_pc_temp = torch.zeros(config.batch_size, 32, 8, 8).to(config.device)
+                ft_DE_pc_temp = torch.zeros(config.batch_size,64,4,4).to(config.device)
+
+                ft_AB_pc_temp,ft_BC_pc_temp,ft_CD_pc_temp,ft_DE_pc_temp,ft_EF_pc_temp,ft_FG_ppc_temp,output = net.feedforward_pass(images,ft_AB_pc_temp,ft_BC_pc_temp,ft_CD_pc_temp,ft_DE_pc_temp)
+                
+                _,predicted=torch.max(output,1)
+                total_correct[0]+=(predicted==labels).sum().item()
+
+                # In pc_train.py training loop
+                ft_AB_pc_temp.requires_grad_(True)
+                ft_BC_pc_temp.requires_grad_(True)
+                ft_CD_pc_temp.requires_grad_(True)
+                ft_DE_pc_temp.requires_grad_(True)
+
+                optimizer.zero_grad()
+                final_loss=0
+                train_recon_loss=0
+                for i in range(config.timesteps):
+                    output,ft_AB_pc_temp,ft_BC_pc_temp,ft_CD_pc_temp,ft_DE_pc_temp,ft_EF_pc_temp,loss_of_layers=net.predictive_coding_pass(images,ft_AB_pc_temp,ft_BC_pc_temp,ft_CD_pc_temp,ft_DE_pc_temp,ft_EF_pc_temp,config.betaset,config.gammaset,config.alphaset,images.size(0))
+                    loss=criterion(output,labels)
+                    final_loss+=loss
+                    train_recon_loss+=loss_of_layers
+                    _,predicted=torch.max(output,1)
+                    total_correct[i+1]+=(predicted==labels).sum().item()
+
+                total_samples+=labels.size(0)
+                final_loss=final_loss/config.timesteps
+                train_recon_loss=train_recon_loss/config.timesteps
+                final_loss.backward()
+                optimizer.step()
+                running_loss.append(final_loss.item())
+                val_recon_loss.append(train_recon_loss.item())
+
+                #Clear Batches
+                del ft_AB_pc_temp, ft_BC_pc_temp, ft_CD_pc_temp, ft_DE_pc_temp,ft_EF_pc_temp,loss_of_layers
+                torch.cuda.empty_cache()
+            
+            accuracy=[100 * c /total_samples for c in total_correct]
+            accuracy=np.array(accuracy)
+            train_accuracy=np.mean(accuracy)
+            avg_loss=np.mean(running_loss)
+            avg_recon_loss=np.mean(val_recon_loss)
+            print(f"Epoch:{epoch} and AverageLoss:{avg_loss} and Reconstruction Loss:{train_recon_loss}")
+            net.eval()
+            test_accuracy,test_loss,test_recon_loss=eval_pc_accuracy(net,testloader,config,criterion)
+            #metrics={"Fine_Tuning_With_Classification/train_loss":avg_loss,"Fine_Tuning_With_Classification/test_loss":test_loss,"Fine_Tuning_With_Classification/Test_Accuracy":test_accuracy,"Fine_Tuning_With_Classification/Training_accuracy":train_accuracy,"Fine_Tuning_With_Classification/Recon_Training_loss":avg_recon_loss,"Fine_Tuning_With_Classification/Recon_Testing_loss":test_recon_loss }
+            #wandb.log(metrics)
+            #loss_arr.append(avg_loss)
+
+        return True
+
---
+++ week9/main.py
@@ -0,0 +1,250 @@
+# Import necessary libraries
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import Dataset
+from torch.utils.tensorboard import SummaryWriter
+from network import Net as Net
+from fwd_train import feedfwd_training
+from back_train import feedback_training
+from pc_train import class_pc_training
+from recon_pc_train import recon_pc_training
+from eval_and_plotting import eval_pc_accuracy,recon_pc_loss
+from illusion_pc_train import illusion_pc_training
+from customdataset import SquareDataset
+import random
+import os
+import sys
+import time
+from wb_tracker import init_wandb
+import wandb
+import argparse
+import importlib
+from PIL import Image
+import torchvision.utils as vutils
+import pandas as pd
+
+start = time.time()
+
+classes = (
+    'plane',
+    'car',
+    'bird',
+    'cat',
+    'deer',
+    'dog',
+    'frog',
+    'horse',
+    'ship',
+     'truck')
+
+def set_seed(seed):
+
+    # for random module
+    random.seed(seed)
+    # for numpy
+    np.random.seed(seed)
+    # for cpu
+    torch.manual_seed(seed)
+    # for gpus
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    # for reproducibility
+    torch.backends.cudnn.deterministic = True
+    # disable auto-optimization
+    torch.backends.cudnn.benchmark = False
+
+
+
+
+def train_test_loader(datasetpath,illusion_bool):
+    # Normalizing the images
+    #Andrea's nromalization is different figure out why
+
+    transform = transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616))])
+
+    if illusion_bool == True:
+        DATA_DIR = datasetpath
+        trainset=SquareDataset(os.path.join(DATA_DIR, "metadata.csv"), DATA_DIR,classes_for_use=["Square", "Random"],transform=transform)
+        trainloader=torch.utils.data.DataLoader(trainset, batch_size=config.batch_size, shuffle=True, num_workers=0)
+        testset=SquareDataset(os.path.join(DATA_DIR, "metadata.csv"), DATA_DIR,classes_for_use=["Square", "Random", "All-in", "All-out"],transform=transform)
+        testloader=torch.utils.data.DataLoader(testset, batch_size=config.batch_size, shuffle=False, num_workers=0)
+
+    else:
+
+        trainset = torchvision.datasets.CIFAR10(
+        root=datasetpath,
+        train=True,
+        download=True,
+        transform=transform)
+
+        trainloader = torch.utils.data.DataLoader(
+        trainset, batch_size=config.batch_size, shuffle=True, num_workers=0)
+
+        testset = torchvision.datasets.CIFAR10(
+        root=datasetpath,
+        train=False,
+        download=True,
+        transform=transform)
+
+        testloader = torch.utils.data.DataLoader(
+        testset, batch_size=config.batch_size, shuffle=False, num_workers=0)
+
+    return trainloader, testloader
+
+
+def training_using_reconstruction_and_predicitve_coding(save_dir, trainloader, testloader,config,iteration_index):
+    net = Net(num_classes=10).to(config.device)
+    if iteration_index != 0:
+        checkpoint = torch.load("{config.load_model_path}/{config.model_name}_{iteration_index}.pth")
+        net.conv1.load_state_dict(checkpoint["conv1"])
+        net.conv2.load_state_dict(checkpoint["conv2"])
+        net.conv3.load_state_dict(checkpoint["conv3"])
+        net.conv4.load_state_dict(checkpoint["conv4"])
+        net.deconv1_fb.load_state_dict(checkpoint["deconv1_fb"])
+        net.deconv2_fb.load_state_dict(checkpoint["deconv2_fb"])
+        net.deconv3_fb.load_state_dict(checkpoint["deconv3_fb"])
+        net.deconv4_fb.load_state_dict(checkpoint["deconv4_fb"])
+
+    recon_pc_training(net,trainloader,testloader,"train",config)
+
+    # Save only conv layers
+    torch.save({
+        "conv1": net.conv1.state_dict(),
+        "conv2": net.conv2.state_dict(),
+        "conv3": net.conv3.state_dict(),
+        "conv4": net.conv4.state_dict(),
+        "deconv1_fb": net.deconv1_fb.state_dict(),
+        "deconv2_fb": net.deconv2_fb.state_dict(),
+        "deconv3_fb": net.deconv3_fb.state_dict(),
+        "deconv4_fb": net.deconv4_fb.state_dict(),
+
+    }, f'{config.save_model_path}/{config.model_name}_{iteration_index + 1 }.pth')
+
+    print("Model Save Sucessfully")
+
+    return True
+
+
+def fine_tuning_using_classification(save_dir, trainloader, testloader,config,iteration_index):
+    
+    net = Net(num_classes=10).to(config.device)
+    if iteration_index == 0:
+        checkpoint = torch.load("{config.load_model_path}/{config.model_name}_{iteration_index}.pth")
+        net.conv1.load_state_dict(checkpoint["conv1"])
+        net.conv2.load_state_dict(checkpoint["conv2"])
+        net.conv3.load_state_dict(checkpoint["conv3"])
+        net.conv4.load_state_dict(checkpoint["conv4"])
+        net.deconv1_fb.load_state_dict(checkpoint["deconv1_fb"])
+        net.deconv2_fb.load_state_dict(checkpoint["deconv2_fb"])
+        net.deconv3_fb.load_state_dict(checkpoint["deconv3_fb"])
+        net.deconv4_fb.load_state_dict(checkpoint["deconv4_fb"])
+
+else:
+        net.load_state_dict(torch.load(f'{config.load_model_path}/{config.model_name}_{iteration_index}.pth',map_location=config.device,weights_only=True))
+
+    train_bool=recon_pc_training(net,trainloader,testloader,"fine_tuning",config)
+
+    if train_bool == True:
+        torch.save(net.state_dict(), f'{config.save_model_path}/{config.model_name}_{iteration_index + 1 }.pth')
+        print("Model Saved Sucessfully")
+
+
+    return train_bool
+
+
+def fine_tuning_using_illusions(save_dir, trainloader, testloader,config,iteration_index):
+
+    net = Net(num_classes=2).to(config.device)
+
+    if iteration_index == 0:
+        checkpoint = torch.load("{config.load_model_path}/{config.model_name}_{iteration_index}.pth")
+        net.conv1.load_state_dict(checkpoint["conv1"])
+        net.conv2.load_state_dict(checkpoint["conv2"])
+        net.conv3.load_state_dict(checkpoint["conv3"])
+        net.conv4.load_state_dict(checkpoint["conv4"])
+        net.deconv1_fb.load_state_dict(checkpoint["deconv1_fb"])
+        net.deconv2_fb.load_state_dict(checkpoint["deconv2_fb"])
+        net.deconv3_fb.load_state_dict(checkpoint["deconv3_fb"])
+        net.deconv4_fb.load_state_dict(checkpoint["deconv4_fb"])
+
+else:
+        net.load_state_dict(torch.load(f'{config.load_model_path}/{config.model_name}_{iteration_index}.pth',map_location=config.device,weights_only=True))
+
+    train_bool=illusioin_pc_training(net,trainloader,testloader,"fine_tuning",config)
+
+    if train_bool == True:
+        torch.save(net.state_dict(), f'{config.save_model_path}/{config.model_name}_{iteration_index + 1 }.pth')
+        print("Model Saved Sucessfully")
+
+
+    return train_bool
+
+
+
+def decide_training_model(condition,save_dir, trainloader, testloader, config,iteration_index):
+    cond_to_func={
+            "recon_pc_train":lambda: training_using_reconstruction_and_predicitve_coding(save_dir, trainloader, testloader, config,iteration_index),
+            "fine_tuning_classification": lambda:fine_tuning_using_classification(save_dir, trainloader, testloader,config,iteration_index),
+            "random_network_testing": lambda:reconstruction_testing_on_random_network(save_dir, trainloader, testloader,config,iteration_index),
+            "illusion_train": lambda:fine_tuning_using_illusions(save_dir, trainloader, testloader,config,iteration_index),
+            "recon_comparison": lambda:recon_vs_original(testloader, config, n_images=8, iteration_index=15) ,
+            "feedforward_training": lambda:}
+
+    True=cond_to_func[condition]()
+
+    return None
+
+
+
+def main():
+    init_wandb(config.batch_size,config.epochs,config.lr,config.momentum,config.seed,config.device,config.training_condition,config.timesteps,config.gammaset,config.betaset,config.alphaset,config.datasetpath,config.experiment_name,config.noise_type,config.noise_param,config.model_name)
+
+    trainloader, testloader = train_test_loader(config.datasetpath,config.illusion_dataset_bool)
+    for iteration_index in range(config.iterations):
+        if config.training_condition == None:
+            break
+        print(f"The Iteration{iteration_index}:")
+        print("================================")
+        decide_training_model(config.training_condition, save_dir, trainloader, testloader, config,iteration_index)
+        
+
+
+
+
+
+
+
+
+# This line ensures safe multiprocessing
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", type=str, required=True)
+    args = parser.parse_args()
+
+    # Add config directory to Python path
+    sys.path.append(os.path.abspath("configs"))
+
+    config = load_config(args.config)
+
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
