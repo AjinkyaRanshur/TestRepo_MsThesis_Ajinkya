97ece8b46333379a2a5973aeee3f603566842b0c
AjinkyaRanshur <ranshur.ajinkya.res@gmail.com>
2025-10-21 19:24:48 +0200

    experiments script added

--- week9/Experiments_Scripts.py
+++ week9/Experiments_Scripts.py
@@ -1,8 +1,56 @@
+import sys
+import os
+from main import load_config, main
 import pyfiglet
-f = pyfiglet.figlet_format("Pred-Net Analyzer",font="ogre")
-print("*"*50)
-print(f)
-print("*"*50)
-print("Please Select one of the Options Listed Below")
-print("")
+
+def bar_plots():
+
+    return None
+
+def traj_plots():
+
+    return None
+
+def traj_plots_ill():
+
+    return None
+
+def run_and_analyze(config_name):
+    # Add config directory to Python path
+    sys.path.append(os.path.abspath("configs"))
+    config=load_config(config_name)
+    results=main(config)
+
+    return results
+
+if __name__ == "__main__":
+    f = pyfiglet.figlet_format("Pred-Net Analyzer",font="ogre")
+    print("="*50)
+    print(f)
+    print("="*50)
+    print("1.) Analyze the Trajectories over Timesteps")
+    print("2.) Perform Pattern Testing on The Model")
+    selection = int(input("Please choose one of the menu options.\n>> "))
+
+    results=run_and_analyze("configilltest")
+
+    if selection ==1:
+        print(results)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
 
