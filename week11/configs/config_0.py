scaling of classification dataset fixed

week11/configs/config_0.py
@@ -2,14 +2,14 @@ import torch
 
 batch_size = 40
 epochs = 200
-seed = 42
+seed = 1825
 lr = 5e-05
 momentum = 0.9
 classification_datasetpath = "custom_illusion_dataset"
-recon_datasetpath = "None"
-training_condition = "classification_training_shapes"
+recon_datasetpath = "custom_illusion_dataset"
+training_condition = "recon_pc_train"
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-classification_neurons = 6
+classification_neurons = 10
 
 print(f"CUDA available: {torch.cuda.is_available()}")
 print(f"CUDA device count: {torch.cuda.device_count()}")
@@ -25,16 +25,13 @@ gammaset = [[0.33, 0.33, 0.33, 0.33]]  # pattern: Uniform
 betaset = [[0.33, 0.33, 0.33, 0.33]]  # pattern: Uniform
 alphaset = [[0.01, 0.01, 0.01, 0.01]]
 
-model_name = "pc_recon10_custom_illusion_dataset_Uniform_seed42_150_custom_illusion_dataset_classt10_custom_illusion_dataset_Uniform_seed42"
+model_name = "recon_t10_ill_uni_s1825"
 noise_type="s&p"
 noise_param=0.0
 
-experiment_name = "Testing pc_recon10_custom_illusion_dataset_Uniform_seed42_150_custom_illusion_dataset_classt10_custom_illusion_dataset_Uniform_seed42 with Uniform pattern at 10 timesteps"
+experiment_name = "Testing recon_t10_ill_uni_s1825 with Uniform pattern at 10 timesteps"
 load_model_path="/home/ajinkyar/ml_models"
 save_model_path="/home/ajinkyar/ml_models"
 
 
 
-
-base_recon_model = "pc_recon10_custom_illusion_dataset_Uniform_seed42_150"
-checkpoint_epoch = 150

week11/configs/config_3.py
@@ -2,11 +2,11 @@ import torch
 
 batch_size = 40
 epochs = 200
-seed = 1825
+seed = 410
 lr = 5e-05
 momentum = 0.9
-classification_datasetpath = "cifar10"
-recon_datasetpath = "cifar10"
+classification_datasetpath = "custom_illusion_dataset"
+recon_datasetpath = "custom_illusion_dataset"
 training_condition = "recon_pc_train"
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 classification_neurons = 10
@@ -25,11 +25,11 @@ gammaset = [[0.33, 0.33, 0.33, 0.33]]  # pattern: Uniform
 betaset = [[0.33, 0.33, 0.33, 0.33]]  # pattern: Uniform
 alphaset = [[0.01, 0.01, 0.01, 0.01]]
 
-model_name = "recon_t10_c10_uni_s1825"
+model_name = "recon_t10_ill_uni_s410"
 noise_type="s&p"
 noise_param=0.0
 
-experiment_name = "Testing recon_t10_c10_uni_s1825 with Uniform pattern at 10 timesteps"
+experiment_name = "Testing recon_t10_ill_uni_s410 with Uniform pattern at 10 timesteps"
 load_model_path="/home/ajinkyar/ml_models"
 save_model_path="/home/ajinkyar/ml_models"
 